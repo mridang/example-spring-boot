87d2c54ae2406d8e40e210f818202411a9c41c40
Mridang Agarwalla <mridang@nosto.com>
2022-03-11 12:28:46 +0200

    Initial commit

---
+++ app/src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,63 @@
+package com.example.demo;
+
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @RequestMapping(value = "/")
+    public Map<String, String> index(HttpServletRequest req) {
+        return getRequestInformation(req);
+    }
+
+    private Map<String, String> getRequestInformation(HttpServletRequest request) {
+        Map<String, String> map = new HashMap<>();
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String key = headerNames.nextElement();
+            String value = request.getHeader(key);
+            map.put("header: " + key, value);
+        }
+        Enumeration<String> parameterNames = request.getParameterNames();
+        while (parameterNames.hasMoreElements()) {
+            String key = parameterNames.nextElement();
+            String value = request.getParameter(key);
+            map.put("parameter: " + key, value);
+        }
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            map.put("cookie: " + cookie.getName(), cookie.getValue());
+
+        }
+        while (parameterNames.hasMoreElements()) {
+            String key = parameterNames.nextElement();
+            String value = request.getParameter(key);
+            map.put("parameter: " + key, value);
+        }
+        map.put("getRemoteUser", request.getRemoteUser());
+        map.put("getQueryString", request.getQueryString());
+        map.put("getAuthType", request.getAuthType());
+        map.put("getContextPath", request.getContextPath());
+        map.put("getPathInfo", request.getPathInfo());
+        map.put("getPathTranslated", request.getPathTranslated());
+        map.put("getRequestedSessionId", request.getRequestedSessionId());
+        map.put("getRequestURI", request.getRequestURI());
+        map.put("getRequestURL", request.getRequestURL().toString());
+        map.put("getMethod", request.getMethod());
+        map.put("getServletPath", request.getServletPath());
+        map.put("getContentType", request.getContentType());
+        map.put("getLocalName", request.getLocalName());
+        map.put("getProtocol", request.getProtocol());
+        map.put("getRemoteAddr", request.getRemoteAddr());
+        map.put("getServerName", request.getServerName());
+        return map;
+    }
+}
